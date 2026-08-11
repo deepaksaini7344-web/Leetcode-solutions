d7819a7522accd0526a8f7b41a57f1a0766b9b9a
Deepak Saini <deepaksaini7344@gmail.com>
2026-08-11 23:52:52 +0530

    Time: 2 ms (78.8%), Space: 10.1 MB (78.57%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int string_size = s.size();
+        int integer  = 0;
+        for(int i = 0; i<= string_size ; i++){
+        if(s[i] == 'I'){
+            if( i != string_size-1 && s[i+1] == 'V'){
+               integer += 4;
+               i++;
+            }
+           else if( i != string_size-1 && s[i+1] == 'X'){
+               integer += 9;
+               i++;
+            }
+            else {
+                integer += 1;
+            }
+        }
+        else if (s[i] == 'X'){
+             if( i != string_size-1 && s[i+1] == 'L'){
+               integer += 40;
+               i++;
+            }
+           else if( i != string_size-1 && s[i+1] == 'C'){
+               integer += 90;
+               i++;
+            }
+            else {
+                integer += 10;
+            }
+        }
+         else if (s[i] == 'C'){
+             if( i != string_size-1 && s[i+1] == 'D'){
+               integer += 400;
+               i++;
+            }
+           else if( i != string_size-1 && s[i+1] == 'M'){
+               integer += 900;
+               i++;
+            }
+            else {
+                integer += 100;
+            }
+        }
+        else if (s[i]=='V'){
+            integer += 5;
+        }
+         else if (s[i]=='L'){
+            integer += 50;
+        }
+         else if (s[i]=='D'){
+            integer += 500;
+        }
+        else if(s[i] == 'M'){
+            integer += 1000;        
+        }
+    }
+    return integer;
+}
+};
