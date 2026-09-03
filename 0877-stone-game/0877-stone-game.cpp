826665f875ca1b2b0cf29587ef87959fb88a9563
Deepak Saini <deepaksaini7344@gmail.com>
2026-09-03 19:27:24 +0530

    Time: 0 ms (100%), Space: 10.3 MB (85.86%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-return true;
+        int n = piles.size();
+        int i = 0;
+        int j = n - 1;
+        int Alice = 0;
+        int bob = 0;
+        while (n--) {
+            if (n % 2 == 1) {
+                if (piles[i] >= piles[j]) {
+                    Alice += piles[i];
+                    i++;
+                } else {
+                    Alice += piles[j];
+                    j--;
+                }
+            } else {
+                if (piles[i] >= piles[j]) {
+                    bob += piles[j];
+                    j--;
+                } else {
+                    bob += piles[i];
+                    i++;
+                }
+            }
+        }
+        if(Alice > bob) return true;
+        return false;
     }
 };
